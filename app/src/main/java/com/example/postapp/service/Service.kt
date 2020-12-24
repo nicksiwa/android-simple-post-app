25fa1ebd9b92dab85a3de1fcc6d614c5df3b32b0
NATTAPORN AUTTAKUL (ณัฎฐาพร อุทกูล) <5830214038@email.psu.ac.th>
2020-12-24 22:41:05 +0700

    edit service url

--- app/src/main/java/com/example/postapp/service/Service.kt
+++ app/src/main/java/com/example/postapp/service/Service.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceBuilder {
-    private const val baseUrl = "http://172.16.4.134:3000/"
+    private const val baseUrl = "http://10.0.2.2:5000/"
     private val client = OkHttpClient.Builder().build()
     private val retrofit = Retrofit.Builder()
             .baseUrl(baseUrl)
